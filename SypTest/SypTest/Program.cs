5778c5d0a99339f145d4bd4b953a513a44ae5af7
ZachiZachovic <55434118+ZachiZachovic@users.noreply.github.com>
2021-10-19 08:43:45 +0200

    Add files via upload
    TestProjekt

---
+++ SypTest/SypTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SypTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int easyMath()
+        {
+            return 1 + 1;
+        }
+    }
+}
